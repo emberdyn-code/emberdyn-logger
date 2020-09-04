9c9233e28d267a258c1fb6c18131b74ab1b124c1
Emberdyn, LLC <shawn@emberdyn.com>
2020-09-04 08:57:19 -0400

    Added example usage app

---
+++ example/index.js
@@ -0,0 +1,10 @@
+const logger = require('emberdyn-logger')
+
+logger.log({ message: 'Hello World', level: 'error' })
+logger.log({ message: 'Hello World', level: 'event' })
+logger.log({ message: 'Hello World', level: 'warn' })
+logger.log({ message: 'Hello World', level: 'info' })
+logger.log({ message: 'Hello World', level: 'debug' })
+logger.log({ message: 'Hello World', level: 'fatal' })
+logger.log({ message: 'Hello World', level: 'system' })
+logger.log({ message: 'Hello World', level: 'database' })
